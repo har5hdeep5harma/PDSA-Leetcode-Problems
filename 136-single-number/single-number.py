a8a4cbb7d09ceda9cfac3524cfd13957f073d258
Harshdeep Sharma <harsh7251909511@gmail.com>
2025-08-11 20:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (49.87%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def singleNumber(self, nums):
-        d={}
-        for i in range(len(nums)):
-            if nums[i] in d.keys():
-                d[nums[i]]+=1
-            else:
-                d[nums[i]]=1
-        for key in d.keys():
-            if d[key]==1:
-                return key
-        
+        result = 0
+        for num in nums:
+            result ^= num   
+            # XOR accumulates the unique number
+        return result
