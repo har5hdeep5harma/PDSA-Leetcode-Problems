6c8d6ecb7d963809d346270ca049d2f8837c4412
Harshdeep Sharma <harsh7251909511@gmail.com>
2025-08-16 19:58:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (49.11%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def maximum69Number (self, num):
-        a=str(num)
+        a=list(str(num))
         b=""
-        c=True
         for i in range(len(a)):
-            if int(a[i])<9 and c:
-                b+='9'
-                c=False
-            else:
-                b+=a[i]
+            if int(a[i])<9:
+                a[i]='9'
+                break
+        for i in range(len(a)):
+            b+=a[i]
         return int(b)
