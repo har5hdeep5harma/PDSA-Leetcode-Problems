a21bbf0cda18bf841948908597db1ce6bd98270a
Harshdeep Sharma <harsh7251909511@gmail.com>
2025-08-18 22:46:52 +0530

    Time: 8 ms (13.22%) | Memory: 12.5 MB (51.14%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,29 +1,28 @@
 class Solution(object):
-    def isHappy(self, n):
-        # if len(list(s))==0:
-        #     s=set()
-        
-        # if n==1:
-        #     return True
-        # elif n in s:
-        #     return False
-        # else:
-        #     s.add(n)
-        # st=str(n)
-        # sq=0
-        # for i in range(len(st)):
-        #     sq+=int(st[i])
-        # self.isHappy(sq)
-        s=set()
+    def isHappy(self, n, s=None):
+        if s==None:
+            s=set()
+        if n==1:
+            return True
+        elif n in s:
+            return False
+        else:
+            s.add(n)
         st=str(n)
-        while st not in s:
-            s.add(st)
-            sq=0
-            for i in st:
-                sq+=int(i)**2
-            if sq==1:
-                return True
-            st=str(sq)
-        return False
+        sq=0
+        for i in range(len(st)):
+            sq+=int(st[i])**2
+        return self.isHappy(sq, s)
+        # s=set()
+        # st=str(n)
+        # while st not in s:
+        #     s.add(st)
+        #     sq=0
+        #     for i in st:
+        #         sq+=int(i)**2
+        #     if sq==1:
+        #         return True
+        #     st=str(sq)
+        # return False
 
 
