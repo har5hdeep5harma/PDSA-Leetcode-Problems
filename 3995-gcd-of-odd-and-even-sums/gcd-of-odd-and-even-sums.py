9b32f56d52152bea393fe8f8628ac57b78ea88e7
Harshdeep Sharma <harsh7251909511@gmail.com>
2026-06-04 02:27:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (99.48%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -4,5 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        se=n*(n+1)
+        so=n*n
+        m=se
+        n=so
+        r=m%n
 
+        while(r!=0):
+            m=n
+            n=r
+            r=m%n
         return n
