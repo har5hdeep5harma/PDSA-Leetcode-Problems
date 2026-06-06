a98b0c32ce07378d4a0e1a67575ded9ebf31127c
Harshdeep Sharma <harsh7251909511@gmail.com>
2026-06-06 10:20:08 +0530

    Time: 2 ms (88.97%) | Memory: 13.5 MB (73.79%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,11 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        a=list(set(nums))
-        max=0
-        m1=0
-        for i in a:
-            if nums.count(i)>max:
-                m1=i
-                max=nums.count(i)
-        return(m1)
+        # a=list(set(nums))
+        # max=0
+        # m1=0
+        # for i in a:
+        #     if nums.count(i)>max:
+        #         m1=i
+        #         max=nums.count(i)
+        # return(m1)
+        
+        d={}
+        count=0
+        c=0
+        for i in nums:
+            if count==0:
+                c=i
+            
+            if i==c:
+                count+=1
+            else:
+                count-=1
+        return c
+
