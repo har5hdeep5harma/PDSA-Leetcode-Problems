db3c8ad28a1b7da691ff30b7c36fc0b1eca99ad2
Harshdeep Sharma <harsh7251909511@gmail.com>
2026-06-06 10:36:57 +0530

    Time: 15 ms (70.78%) | Memory: 12.6 MB (77.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,11 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        a=""
-        for i in range(len(s)):
-            if s[i].isalnum():
-                a+=s[i]
-        a=a.lower()
+        # a=""
+        # for i in range(len(s)):
+        #     if s[i].isalnum():
+        #         a+=s[i]
+        # a=a.lower()
 
-        return a==a[::-1]
+        # return a==a[::-1]
         
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+                
+            if s[left].lower() != s[right].lower():
+                return False
+                
+            left += 1
+            right -= 1
+            
+        return True
